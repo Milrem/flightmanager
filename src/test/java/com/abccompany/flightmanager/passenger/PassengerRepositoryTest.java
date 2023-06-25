800755177a999acbfabcc6712a42ea1a2e0f8fd9
milrem <cris.a.neves@gmail.com>
2023-06-25 17:45:17 -0300

    Using AssertJ to do the same assentions but with another style

--- src/test/java/com/abccompany/flightmanager/passenger/PassengerRepositoryTest.java
+++ src/test/java/com/abccompany/flightmanager/passenger/PassengerRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.abccompany.flightmanager.passenger;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -19,6 +20,14 @@ class PassengerRepositoryTest {
         Optional<Passenger> found = passengerRepository.findByName(name);
         assertTrue(found.isPresent());
         assertEquals(name, found.get().getName());
+        assertTrue(found.get().isVip());
+
+        Assertions.assertThat(found)
+                .isNotEmpty()
+                .get()
+                .hasFieldOrProperty("id")
+                .hasFieldOrPropertyWithValue("name", name)
+                .hasFieldOrPropertyWithValue("vip", true);
 
         name = "Jonh Smith";
         found = passengerRepository.findByName(name);
