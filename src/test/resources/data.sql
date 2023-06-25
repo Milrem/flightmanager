c1ee035e6eeee389fa6f20bffa212589ba921031
milrem <cris.a.neves@gmail.com>
2023-06-25 17:47:52 -0300

    Prepara additional data for the integration tests

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,1 +1,5 @@
+INSERT INTO Passenger (id, name, vip) VALUES ('P8', 'Adriana Peres', FALSE);
 INSERT INTO Passenger (id, name, vip) VALUES ('P9', 'Ely Nogueira', FALSE);
+
+INSERT INTO Flight (id, category) VALUES ('E8', 'BUSINESS');
+INSERT INTO Flight (id, category) VALUES ('E9', 'ECONOMIC');
